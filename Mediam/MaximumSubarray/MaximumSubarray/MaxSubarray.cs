e2d67e162088939f174f561e168123c29a2f768e
Mahmoud Hosni <mah1998mod@gmail.com>
2023-05-16 21:40:27 +0300

    Maximum product subarray

---
+++ Mediam/MaximumSubarray/MaximumSubarray/MaxSubarray.cs
@@ -0,0 +1,27 @@
+﻿
+
+using System.Globalization;using System.Text.Json.Serialization.Metadata;
+
+namespace MaximumSubarray
+{
+    public class MaxSubrray
+    {
+        public static int MaxSubArray(List<int> nums)
+        {
+            int maxSub = nums[0];
+            int curSum = 0;
+
+            foreach (int num in nums)
+            {
+                if (curSum < 0)
+                    curSum = 0;
+
+                curSum += num;
+                maxSub = Math.Max(maxSub, curSum);
+            }
+
+            return maxSub;
+        }
+    }
+}
+
---
+++ Mediam/MaximumSubarray/MaximumSubarray/Program.cs
@@ -0,0 +1,5 @@
+﻿using MaximumSubarray;
+
+var nums = new List<int> { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+
+Console.WriteLine(MaxSubrray.MaxSubArray(nums));
