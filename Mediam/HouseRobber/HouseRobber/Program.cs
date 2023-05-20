14828b9b871c2041b675e9c7e3e8c9fd54f01eba
Mahmoud Hosni <mah1998mod@gmail.com>
2023-05-20 22:04:32 +0300

    added house robber

---
+++ Mediam/HouseRobber/HouseRobber/Program.cs
@@ -0,0 +1,4 @@
+﻿using HouseRobber;
+
+var test = new int[] {1,5,6,4,3,2};
+Console.WriteLine(Solution.Rob(test));
---
+++ Mediam/HouseRobber/HouseRobber/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRobber
+{
+    public class Solution
+    {
+        public static int Rob(int[] nums)
+        {
+            if (nums == null) return 0;
+            if (nums.Length == 1) return nums[0];
+            int previouslyRobbedMoney = 0;
+            int robbedMoneyBeforePreviousRob = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int currentRobValue = Math.Max(previouslyRobbedMoney, nums[i] + robbedMoneyBeforePreviousRob);
+                robbedMoneyBeforePreviousRob = previouslyRobbedMoney;
+                previouslyRobbedMoney = currentRobValue;
+            }
+            return previouslyRobbedMoney;
+        }
+    }
+}
