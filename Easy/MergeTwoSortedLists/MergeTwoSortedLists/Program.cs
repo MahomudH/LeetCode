45ec3d11314d2455be763eef0952e1cb3e34120f
Mahmoud Hosni <mah1998mod@gmail.com>
2023-05-23 20:41:46 +0300

    added MergeTwoSortedLists

---
+++ Easy/MergeTwoSortedLists/MergeTwoSortedLists/Program.cs
@@ -0,0 +1,26 @@
+﻿using MergeTwoSortedLists;
+
+ListNode list1 = new ListNode(1)
+{
+    next = new ListNode(3)
+    {
+        next = new ListNode(5)
+    }
+};
+
+ListNode list2 = new ListNode(2)
+{
+    next = new ListNode(4)
+    {
+        next = new ListNode(6)
+    }
+};
+
+ListNode mergedList = MergeTwoSortedList.MergeTwoLists(list1, list2);
+
+Console.WriteLine("Merged List:");
+while (mergedList != null)
+{
+    Console.Write(mergedList.val + " ");
+    mergedList = mergedList.next;
+}
