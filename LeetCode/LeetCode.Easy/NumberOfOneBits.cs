bec01fd9d7372b614c73335796443ed6def214f5
Mahmoud Hosni <mah1998mod@gmail.com>
2023-09-07 21:04:19 +0300

    solve Number Of One Bits problem

---
+++ LeetCode/LeetCode.Easy/NumberOfOneBits.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Easy
+{
+    public class NumberOfOneBits
+    {
+        public int HammingWeight(uint n)
+        {
+            int numberOfOneBits = 0;
+            while (n > 0)
+            {
+                numberOfOneBits += (int)(n % 2);
+                n /= 2;
+            }
+            return numberOfOneBits;
+        }
+    }
+}
