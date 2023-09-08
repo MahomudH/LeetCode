1efc245ac8d40329bcc070fe5ad582826cbae29c
Mahmoud Hosni <mah1998mod@gmail.com>
2023-09-08 07:06:06 +0300

    solve contains duplicate problem

---
+++ LeetCode/LeetCode.Easy/ContainsDuplicate.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.Easy
+{
+    public class ContainsDuplicate
+    {
+        public static bool IsContainsDuplicateV1(int[] nums)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            for (int i = 0, length = nums.Length; i < length; i++)
+            {
+                if (dic.ContainsKey(nums[i]))
+                    return true;
+
+                dic.Add(nums[i], i);
+            }
+            return false;
+        }
+
+        public static bool IsContainsDuplicateV2(int[] nums)
+        {
+            HashSet<int> dic = new HashSet<int>();
+
+            for (int i = 0, length = nums.Length; i < length; i++)
+            {
+                if (dic.Contains(nums[i]))
+                    return true;
+
+                dic.Add(nums[i]);
+            }
+            return false;
+        }
+    }
+}
