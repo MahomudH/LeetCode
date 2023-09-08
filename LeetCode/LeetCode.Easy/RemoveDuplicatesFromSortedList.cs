46ebe1f0d7990799109ca94a3d133d34368b12be
Mahmoud Hosni <mah1998mod@gmail.com>
2023-09-08 08:31:05 +0300

    solve remove duplicated from linked list

---
+++ LeetCode/LeetCode.Easy/RemoveDuplicatesFromSortedList.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Core;
+
+namespace LeetCode.Easy
+{
+    public class RemoveDuplicatesFromSortedList
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head == null || head.Next == null) return head;
+
+            ListNode current = head;
+
+            while (current.Next != null)
+            {
+                if (current.Value == current.Next.Value)
+                {
+                    current.Next = current.Next.Next;
+                }
+                else
+                {
+                    current = current.Next;
+                }
+            }
+
+            return head;
+        }
+    }
+}
