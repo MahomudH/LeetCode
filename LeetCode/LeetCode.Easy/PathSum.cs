e495d7dd565bdb9679e755b1e214ca533ce65578
Mahmoud Hosni <mah1998mod@gmail.com>
2023-09-17 19:26:27 +0300

    solve path sum peoblem

---
+++ LeetCode/LeetCode.Easy/PathSum.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Core;
+
+namespace LeetCode.Easy;
+
+public class PathSum
+{
+    public static bool HasPathSum(TreeNode root, int targetSum)
+    {
+        if (root == null)
+        {
+            return false;
+        }
+
+        if (root.left == null && root.right == null && targetSum - root.val == 0)
+        {
+            return true;
+        }
+
+        return HasPathSum(root.left, targetSum - root.val) || HasPathSum(root.right, targetSum - root.val);
+    }
+}
+
