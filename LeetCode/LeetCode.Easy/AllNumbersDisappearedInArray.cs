b619e28fec795cbada2c4a710ad1eb3182f7b7e5
Mahmoud Hosni <mah1998mod@gmail.com>
2023-09-19 16:59:10 +0300

    solve All Numbers Disappeared in an Array problem

---
+++ LeetCode/LeetCode.Easy/AllNumbersDisappearedInArray.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Easy;
+
+public class AllNumbersDisappearedInArray
+{
+    public static int[] FindAllNumbersDisappearInArray(int[] nums)
+    {
+        int val;
+        int length = nums.Length;
+        for (int i = 0; i < length; i++)
+        {
+            val = Math.Abs(nums[i]) - 1;
+            nums[val] = -Math.Abs(nums[val]);
+        }
+
+        List<int> result = new List<int>();
+        for (int i = 0; i < length; i++)
+        {
+            if (nums[i] > 0)
+                result.Add(i+1);
+        }
+
+        return result.ToArray();
+    }
+}
+
--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -1,5 +1,8 @@
 ﻿using LeetCode.Easy;
 
-int[] nums = new int[] {2,2,1 };
+int[] nums = new int[] { 4, 3, 2, 7, 8, 2, 3, 1 };
 
-Console.WriteLine(SingleNumber.GetSingleNumber(nums));
+int[] res = AllNumbersDisappearedInArray.FindAllNumbersDisappearInArray(nums);
+
+foreach (int i in res)
+    Console.WriteLine(i);
